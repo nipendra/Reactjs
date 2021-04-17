5a6f9ea9d3a7106d09780e1ec9b90cf72b22639d
nipendra <nipendra.dakshana15@gmail.com>
2021-04-17 23:48:47 +0530

    props

--- src/index.js
+++ src/index.js
@@ -7,38 +7,52 @@ import './index.css'
 // stateless functioal componet
 // always return JSX
 
+const heading = 'This is a mobile list';
 function Mobile(){
     // don't use div instead use React.Fragment or shorthand for this  <>
     return (
      <React.Fragment>
-        <h1>This is a mobile list</h1>
+        <h1>{heading.toUpperCase()}</h1>
        <section className="mobilelist">
-       <Item />
-       <Item />
-       <Item />
-       <Item />
-       <Item />
-       <Item />
-        <Item />
-       <Item />
+       <Item brand="Xiomi"/>
+       <Item brand="Oppo"/>
+       <Item brand="Reno"/>
+       <Item brand="MI"/>
+       <Item brand="None"/>
+       <Item brand="X"/>
         </section>
     </React.Fragment>
     )   // return React.createElement('h1', {} , "hello") 
 }
 
-const Item  = () => {
-    return <article className="item">
-        <Title />
+const Item  = (props) => {
+    // console.log(props);
+    const {brand} = props
+    return (
+        <article className="item">
+        {/* <Title brand={props.brand}/> */}
+        <Title brand={brand} color="red"/>
         <Image />
-    </article>
+         </article>
+    )
 }
 
 const Image = () => {
     return <img src="https://images-eu.ssl-images-amazon.com/images/I/71fumGFEU%2BL._AC_UL115_.jpg" alt="mobile" />
+    
 }
 
-const Title = () => {
-    return <p>Xiaomi Mobile</p>
+// const Title = (props) => {
+//     return <p>{props.brand} Mobile</p>
+// }
+
+// we can also do destructuring of props like this 
+
+
+const Title = ({brand, color}) => {
+
+    return (
+        <p>{brand} Mobile and color is {color}</p>)
 }
 
 ReactDom.render(<Mobile/>, document.getElementById('root'));
